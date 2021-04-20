c43c4f9c269361d8c61171776f2da0d47565db53
Vgsanghera7 <80046955+Vgsanghera7@users.noreply.github.com>
2021-04-20 18:40:48 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,28 @@
+class box{
+    constructor(x, y, width, height) {
+        var options = {
+            "isStatic":false,
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.x=x;
+        this.y=y;
+        World.add(world, this.body);
+      }
+      display(){
+        
+        rectMode(CENTER);
+        fill("grey");
+        stroke("grey");
+        var a=this.body.position;
+        var b=this.body.width;
+        var c=this.body.height;
+
+        rect(a.x,a.y, this.width, this.height);
+    
+      }
+}
